074357fdc2d750a613198f5569c9792e6f03c6ba
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-07 11:07:41 -0300

    Imported Contacus scene

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Navbar from '@/scenes/navbar';
 import Home from '@/scenes/home';
 import Benefits from '@/scenes/benefits';
 import OurClasses from './scenes/ourClasses';
+import ContactUs from './scenes/contactUs';
 
 // Types
 import { SelectedPage } from './shared/types';
@@ -39,6 +40,7 @@ const App = () => {
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
       <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
     </div>
   );
 };
